de36130ec853233ae5f56ac0daf5cd04a8e6494e
Mattia Bombelli <talk@mattiabombelli.com>
2025-01-26 13:06:11 -0800

    Added blog collection. Added two blog articles for testing, and blog homepage. Added barebones structure to display articles in blog homepage. Adde barebones style for single article in blog homepage on mobile.

---
+++ src/blog/second-article/index.md
@@ -0,0 +1,16 @@
+---
+
+title: "This is my second blog article. Let's make the title a little longer."
+datePublished: "2025-01-26"
+slug: "this-is-my-second-blog-article"
+category: "Miscellaneous"
+tags: ["something","this-is-another-tag"]
+featuredImage: "./featured.png"
+excerpt: "I am writing something here as excerpt. I should keep it short, because I will do so for all articles."
+featured: false
+draft: false
+
+---
+
+### This is my first test article, to see if everything is working fine.
+This is just some content, I don't know what to write. Maaaann I feel like I can't properly focus, I don't want to go all the way to the supermarket and I don't want to go to North Van later today. I just wanna chill.
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,6 +2,21 @@ import { defineCollection, z } from "astro:content";
 
 import { glob } from "astro/loaders";
 
+const blog = defineCollection({
+    loader: glob({pattern: "**/*.{md,mdx}", base: "./src/blog/"}),
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        datePublished: z.coerce.date(),
+        slug: z.string(),
+        category: z.string(),
+        tags: z.array(z.string()),
+        featuredImage: image(),
+        excerpt: z.string(),
+        featured: z.boolean(),
+        draft: z.boolean(),
+    })
+});
+
 const illustration = defineCollection({
     loader: glob({pattern: "**/*.{md,mdx}", base: "./src/projects/illustration/"}),
     schema: ({ image }) => z.object({
@@ -16,4 +31,4 @@ const illustration = defineCollection({
     }),
 });
 
-export const collections = { illustration };
+export const collections = { illustration, blog };
