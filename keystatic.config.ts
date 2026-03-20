0c6b7982d2efd93f3625d01fb917061cbaea97b5
Mattia Bombelli <talk@mattiabombelli.com>
2026-03-19 19:13:45 -0700

    Changed keystatic configurations

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { config, fields, collection } from '@keystatic/core'
 
 export default config({
     storage: {
-        kind: 'local',
+        kind: 'github',
+        repo: {
+            owner: 'mattbombelli',
+            name: 'my-website',
+        }    
     },
     collections: {
         illustration: collection({
