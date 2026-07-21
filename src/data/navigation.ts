07587b69d1a4716bb90ce0ee16d30fa9d6a1da80
Mattia Bombelli <talk@mattiabombelli.com>
2026-07-20 22:55:46 -0700

    Added navbar for desktop, laptop, tablet, working on hamburger menu.

---
+++ src/data/navigation.ts
@@ -0,0 +1,5 @@
+export const mainNavLinks = [
+    { href: '/ux', label: 'UX case studies', theme: 'hibiscus' },
+    { href: '/projects', label: 'Projects', theme: 'marmalade' },
+    { href: '/ux', label: 'About me', theme: 'meadow' },
+];
