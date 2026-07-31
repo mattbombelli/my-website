686b1970fb5b1d8bdd337d079a873fc44ab0e3f2
Mattia Bombelli <talk@mattiabombelli.com>
2026-07-31 12:10:06 -0700

    Added blurhash to images. Added correct aspect ratio to Figure to prevent layout shifting.

--- src/utils/resolveImage.ts
+++ src/utils/resolveImage.ts
@@ -1,3 +1,8 @@
+import sharp from 'sharp';
+import { join } from 'node:path';
+import { encode } from 'blurhash';
+import { blurhashToCssGradientString } from '@unpic/placeholder';
+
 export async function resolveImage(path: string | null){
     if (!path) return null;
 
@@ -5,7 +10,35 @@ export async function resolveImage(path: string | null){
         const resolvedPath = decodeURIComponent(path)
             .replace('@images/', '/src/assets/images/');
         const module = await import(/* @vite-ignore */ resolvedPath);
-        return module.default;
+        const { data, info } = await sharp(join(process.cwd(), resolvedPath))
+            .rotate()
+            .raw()
+            .ensureAlpha()
+            .resize(32, 32, { fit: 'inside'})
+            .toBuffer({ resolveWithObject: true });
+
+        let placeholder = null;
+        const aspectRatio = info.width / info.height;
+        const baseComponent = 5;
+
+        let componentX = Math.round(baseComponent * Math.sqrt(aspectRatio));
+        let componentY = Math.round(baseComponent / Math.sqrt(aspectRatio));
+
+        componentX = Math.min(9, Math.max(1, componentX));
+        componentY = Math.min(9, Math.max(1, componentY));
+
+        try {
+            const hash = encode(new Uint8ClampedArray(data), info.width, info.height, componentX, componentY);
+
+            placeholder = blurhashToCssGradientString(hash, componentX, componentY);
+        } catch(e) {
+            console.log(`Could not generate hash for image: ${resolvedPath}`);
+        }
+
+        return {
+            ...module.default,
+            placeholder
+        };
     } catch(e){
         console.error(`Could not resolve image: ${path}`);
         return null;
