15cacf98d6c84f35a2c90472d884651edae09aba
Mattia Bombelli <talk@mattiabombelli.com>
2026-08-12 15:17:19 -0700

    Added more content to Forvo case study

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -160,9 +160,8 @@ export default config({
                       { label: "Left", value: "1" },
                       { label: "Middle", value: "2" },
                       { label: "Right", value: "3" },
-                      { label: "Full width", value: "4" },
                     ],
-                    defaultValue: "2",
+                    defaultValue: "1",
                   }),
                   rowSpan: fields.select({
                     label: "Rows",
@@ -205,7 +204,6 @@ export default config({
                   columnSpan: fields.select({
                     label: "Columns",
                     options: [
-                      { label: "span 1", value: "1" },
                       { label: "span 2", value: "2" },
                       { label: "span 3", value: "3" },
                       { label: "span 4", value: "4" },
@@ -215,11 +213,11 @@ export default config({
                   columnOffset: fields.select({
                     label: "Column offset",
                     options: [
-                      { label: "1", value: "1" },
-                      { label: "2", value: "2" },
-                      { label: "3", value: "3" },
+                      { label: "Left", value: "1" },
+                      { label: "Middle", value: "2" },
+                      { label: "Right", value: "3" },
                     ],
-                    defaultValue: "2",
+                    defaultValue: "1",
                   }),
                   rowSpan: fields.select({
                     label: "Rows",
